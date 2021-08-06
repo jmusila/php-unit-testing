e9b2aa45272e384b0e7e69a17730ea4181abc177
jmusila <jonathanmusila6@gmail.com>
2021-08-06 15:11:53 +0300

    added functions

---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testAddingTwoPlusTwoReusltsInFour()
+    {
+        $this->assertEquals(4, 2+2);
+    }
+}
