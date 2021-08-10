23df6ce2775a6ae0e294c4895ee76ea6ae4d1bcf
jonathanmusila <jonathanmusila6@gmail.com>
2021-08-10 14:41:58 +0300

    added pop and getcount

--- src/Queue.php
+++ src/Queue.php
@@ -8,4 +8,14 @@ class Queue
     {
         $this->items[] = $item;
     }
+
+    public function pop()
+    {
+        return array_pop($this->items);
+    }
+
+    public function getCount()
+    {
+        return count($this->items);
+    }
 }
