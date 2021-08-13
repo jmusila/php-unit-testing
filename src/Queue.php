a3f00ca3534ec6d792286f5d30933e6b4dcb66d3
jmusila <jonathanmusila6@gmail.com>
2021-08-13 16:10:21 +0300

    added queue exception

--- src/Queue.php
+++ src/Queue.php
@@ -9,7 +9,7 @@ class Queue
     public function push($item)
     {
         if ($this->getCount() == static::MAX_ITEMS) {
-            throw new Exception("Queue is full");
+            throw new QueueException("Queue is full");
         }
         $this->items[] = $item;
     }
