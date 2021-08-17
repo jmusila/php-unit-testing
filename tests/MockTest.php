916722e98d9af10767bba5b18582f53e06807f13
jmusila <jonathanmusila6@gmail.com>
2021-08-17 16:28:50 +0300

    added mock test class

--- tests/MockTest.php
+++ tests/MockTest.php
@@ -4,5 +4,10 @@ use PHPUnit\Framework\TestCase;
 
 class MockTest extends TestCase
 {
-    
+    public function testMock()
+    {
+        $mailer = new Mailer();
+
+        $result = $mailer->sendMessage('johndoe@gmail.com', 'Hello');
+    }
 }
